d7a535413103ebcb3373131beb84b555dfedb0b4
Gurp <kooner@gmail.com>
2019-04-19 01:31:20 +0000

    add basic auth

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
     def index
         @categories = Category.all
-        puts "--------------"
-        puts @categories.inspect
-        puts "--------------"
-
       end
     
       def new
